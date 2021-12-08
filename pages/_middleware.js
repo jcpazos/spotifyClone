a88f0ddc01232b315daa46d2dbabbaf8a8a20d84
Jose Carlos Pazos <jpazos@wolfram.com>
2021-12-08 13:22:17 -0500

    add debug statements

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -7,7 +7,7 @@ export async function middleware(req) {
     const {pathname } = req.nextUrl;
     // Allow the requests if the following is true...
     // the token exists
-    console.log(token);
+    console.log("token", token);
     if (pathname.includes("/api/auth/") || token) {
         return NextResponse.next();
     }
